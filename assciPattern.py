83e612913daffd12a58dcc6849b9f1d39722f35f
Zhaochun Wang <carmichael901230@gmail.com>
2019-05-23 19:49:53 -0400

    added something

--- assciPattern.py
+++ assciPattern.py
@@ -2,6 +2,9 @@
 import pandas as pd
 import numpy as np
 import math
+from matplotlib import pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import FactorAnalysis as PCA
 #%%
 raw_file_loc = './datasets/self-efficacy.xlsx'
 
@@ -12,18 +15,14 @@ for i in range(len(init_db.columns)-2):
     header.append(i)
 #%%
 init_db.columns=header
-init_db
 #%%
 ''' Extract [id],[time],[data] from init_db '''
 
 id_table = init_db.loc[:,'id']
-id_table
 #%%
 time_table = init_db.loc[:, 'time']
-time_table
 #%%
 data = init_db.drop(columns=['id','time'])
-data
 #%%
 numRow = len(data.index)
 numCol = len(data.columns)
@@ -34,7 +33,6 @@ for j in range(numCol):
     for i in range(numRow):
         sum += data.iloc[i,j]
     mean.append(sum/numRow)
-mean
 #%%
 ''' Caclulate Sample SD '''
 standDev = []
@@ -43,16 +41,52 @@ for j in range(numCol):
     for i in range(numRow):
         varSum += (data.iloc[i,j]-mean[j])**2
     standDev.append(math.sqrt(varSum/(numRow-1)))
-standDev
 #%%
 # data.loc[len(data)] = mean
 data.rename(index={numRow:"avg"},inplace=True)
-data
 #%%
 # data.loc[len(data)] = standDev
 data.rename(index={numRow+1:"sd"},inplace=True)
-data
+
+#%%
+cols = data.columns
+belief_df = pd.DataFrame()
+action_df = pd.DataFrame()
+#%%
+for col_index in range(len(cols)):
+    if col_index%2 == 0:
+        belief_df[col_index] = data[cols[col_index]]
+    else:
+        action_df[col_index] = data[cols[col_index]]
+
 
 #%%
 
+x = []
+y = []
+print('%20s|%15s'%("number of components", 'total variance'))
+for n in reversed(range(1, 22)):
+    scaled_action_df = StandardScaler().fit_transform(action_df)
+    # print(scaled_action_df)
+    numcol = len(belief_df.columns)
 
+    pca = PCA(n_components=n)
+    pc = pca.fit_transform(scaled_action_df)
+    pcDF = pd.DataFrame(pc)
+    pcDF
+    
+    x.append(n)
+    # ratios = pca.explained_variance_ratio_
+    sum = 0
+    for ratio in ratios:
+        sum += ratio
+    print('%20d|%15f'%(n, sum*100))
+    y.append((sum)*100)
+    # print("=================")
+#%%
+plt.xlabel("number of components")
+plt.ylabel("total Variance")
+
+# plt.plot(x,y)
+# pca.explained_variance_ratio_
+#%%
